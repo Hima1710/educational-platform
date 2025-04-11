67f59a3c0441ba3d94a1e44ea42b8096af75c9d5
Ibrahim Mabrouk <ie4951611@gmail.com>
2025-04-11 22:58:56 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import '@fontsource/cairo/400.css';
 import '@fontsource/cairo/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 ); 
